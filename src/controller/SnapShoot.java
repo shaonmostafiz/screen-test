1504938dd21562fda0806ff03894d0d39835d963
Faisal Islam <faisal@nascenia.com>
2014-03-07 18:20:53 +0600

    separate codes, and apply anytime span shoot methodology

--- src/controller/SnapShoot.java
+++ src/controller/SnapShoot.java
@@ -1,8 +1,17 @@
 package controller;
 
 
+import model.BaseModel;
+import sun.misc.BASE64Encoder;
+
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class SnapShoot {
 
@@ -20,4 +29,42 @@ public class SnapShoot {
 
     }
 
+    public void sendSnapShoot(BufferedImage imageContent) throws Exception {
+
+        BaseModel baseModel = new BaseModel();
+
+        try {
+            //immediately send it to server
+            String imageString = null;
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+            try {
+
+                ImageIO.write(imageContent, "png", bos);
+
+                byte[] imageBytes = bos.toByteArray();
+
+                BASE64Encoder encoder = new BASE64Encoder();
+
+                imageString = encoder.encode(imageBytes);
+
+                bos.close();
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+
+            //System.out.println(imageString);
+            Map<String,String> key_value_pair = new HashMap<String, String>();
+
+            key_value_pair.put("action","uploadSnap");
+            key_value_pair.put("imageString",imageString);
+
+            String response = baseModel.webservice("http://localhost/screen-test/upload.php",BaseModel.REQUEST_POST,key_value_pair);
+        } catch (Exception e) {
+
+          }
+    }
 }
